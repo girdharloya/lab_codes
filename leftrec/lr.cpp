d5acbfeb25800d55d7e788897fb150472d760f1a
girdharloya <girdharloya@gmail.com>
2016-04-29 11:22:32 +0530

    Added files via upload

---
+++ leftrec/lr.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+map <string,vector<string> > m;
+map <string,vector<String> >::iterator f,smi;
+int main(){
+	string input;
+	while(1){
+		cin>>input;
+		if(input[0]=='-')	break;
+		string key=input.substr(0,input.find('-'));
+		//setting that to 1 means it sused
+		NT[key[0]-'A']=1;
+		int next,prev=
+		
+	}
+
+}
+
